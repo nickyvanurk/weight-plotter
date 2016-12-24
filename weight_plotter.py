6c962a6ed004bf0c6e3c4c9c8ab330811730374c
nickyvanurk <nickyvanurk@gmx.com>
2016-12-24 16:12:08 +0100

    Code refactoring

--- weight_plotter.py
+++ weight_plotter.py
@@ -3,28 +3,37 @@ import sys
 import json
 import matplotlib.pyplot as plt
 
-def main():
-  if len(sys.argv) > 1:
-    f = open('data.json', 'r')
-    d = json.load(f)
-    f.close()
-    f = open('data.json', 'w')
-    d.insert(len(d), float(sys.argv[1]))
-    json.dump(d, f)
-    f.close()
-  else:
-    f = open('data.json', 'r')
-    weight = json.load(f)
-    f.close()
-    if len(weight) == 0:
-      return
-    weeks = list(range(len(weight)))
+def has_argument():
+  return len(sys.argv) > 1
 
-    plt.plot(weeks, weight, marker='o')
-    plt.xlabel('Weeks')
-    plt.ylabel('Weight')
-    plt.title('Weight Plotter')
-    plt.show()
+def get_data(file):
+  with open(file, 'r') as f:
+    try:
+      return json.load(f)
+    except:
+      return []
+
+def write_data(data, file):
+  with open(file, 'w') as f:
+    json.dump(data, f)
+
+def add_weight(weight, file):
+  d = get_data(file)
+  d.insert(len(d), weight)
+  write_data(d, file)
+
+def main():
+  if has_argument():
+    add_weight(float(sys.argv[1]), 'data.json')
+  else:
+    weight = get_data('data.json')
+    if weight:
+      weeks = list(range(len(weight)))
+      plt.plot(weeks, weight, marker='o')
+      plt.xlabel('Weeks')
+      plt.ylabel('Weight')
+      plt.title('Weight Plotter')
+      plt.show()
 
 if __name__ == '__main__':
   sys.exit(main())
