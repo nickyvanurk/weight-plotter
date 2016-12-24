0a5e7d5a858d5a2760eed839576850b35ca35056
nickyvanurk <nickyvanurk@gmx.com>
2016-12-24 20:24:18 +0100

    Added optional arguments for goal plot

--- weight_plotter.py
+++ weight_plotter.py
@@ -2,9 +2,19 @@
 import sys
 import json
 import matplotlib.pyplot as plt
+import parser
+import argparse
+import collections
+import numpy
 
-def has_argument():
-  return len(sys.argv) > 1
+def get_command_line_args():
+  defaults = {'weight': '', 'goal': '', 'weeks': ''}
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-c', dest='weight', help='your current weight')
+  parser.add_argument('-g', dest='goal', help='your ideal weight')
+  parser.add_argument('-w', dest='weeks', help='weeks until goal')
+  command_line_args = {k:v for k, v in vars(parser.parse_args()).items() if v}
+  return collections.ChainMap(command_line_args, defaults)
 
 def get_data(file):
   with open(file, 'r') as f:
@@ -23,16 +33,28 @@ def add_weight(weight, file):
   write_data(d, file)
 
 def main():
-  if has_argument():
-    add_weight(float(sys.argv[1]), 'data.json')
+  args = get_command_line_args()
+  current_weight = args['weight'].strip()
+  ideal_weight = args['goal'].strip()
+  weeks_until_goal = args['weeks'].strip()
+
+  if current_weight:
+    add_weight(float(current_weight), 'data.json')
   else:
     weight = get_data('data.json')
     if weight:
       weeks = list(range(len(weight)))
-      plt.plot(weeks, weight, marker='o')
+      if ideal_weight and weeks_until_goal:
+        step_size = (float(ideal_weight) - weight[0]) / int(weeks_until_goal)
+        data = list(numpy.arange(weight[0], float(ideal_weight) + step_size, step_size))
+        plt.plot(list(range(int(weeks_until_goal)+1)), data, marker='o',
+                                                             linestyle='--',
+                                                             label='Goal')
+      plt.plot(weeks, weight, marker='o', label='Weight')
       plt.xlabel('Weeks')
       plt.ylabel('Weight')
       plt.title('Weight Plotter')
+      plt.legend(loc=2)
       plt.show()
 
 if __name__ == '__main__':
